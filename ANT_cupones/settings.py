7e698835d2ef7ce03c67fd6df4c521a272438b40
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-11-03 09:51:48 -0300

    model serializer views routers added

--- ANT_cupones/settings.py
+++ ANT_cupones/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coupons.apps.CouponsConfig',
+    'rest_framework',
+    'coupons',
 ]
 
 MIDDLEWARE = [
